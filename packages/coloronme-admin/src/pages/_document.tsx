9a7281be4c2ad18fc4af98b7814c71267eaabbcc
Han-D-Peter <secret_wings@naver.com>
2023-09-25 18:53:13 +0900

    chore

--- packages/coloronme-admin/src/pages/_document.tsx
+++ packages/coloronme-admin/src/pages/_document.tsx
@@ -5,7 +5,6 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="manifest" href="/manifest.json" />
-        <link rel="apple-touch-icon" href="/icons/icon-192x192.png"></link>
       </Head>
       <body>
         <Main />
