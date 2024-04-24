d691edee62aa65e5380e24f111c9c44b0264f9e1
365support <365support@naver.com>
2024-04-24 14:23:22 +0900

    feat: 자동 줌인 방지

--- packages/coloronme-client/src/pages/_document.tsx
+++ packages/coloronme-client/src/pages/_document.tsx
@@ -4,7 +4,9 @@ import GlobalStyle from '../style/GlobalStyle';
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+      </Head>
       <body>
         <GlobalStyle />
         <Main />
