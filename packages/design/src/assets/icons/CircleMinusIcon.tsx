d677bc02837a7b0b07844393b6f4c632eeecde9c
365support <365support@naver.com>
2024-05-23 09:18:06 +0900

    fix: 아이콘 camelcase 로 변경

--- packages/design/src/assets/icons/CircleMinusIcon.tsx
+++ packages/design/src/assets/icons/CircleMinusIcon.tsx
@@ -4,7 +4,7 @@ export function CircleMinusIcon({ width, height, color = '#1B1B1B' }: CustomSVGP
   return (
     <svg xmlns="http://www.w3.org/2000/svg" width={width} height={height} viewBox="0 0 16 16" fill="none">
       <circle cx="8" cy="8" r="8" fill={color} />
-      <path d="M4 8H12" stroke="white" stroke-width="2" stroke-linecap="round" />
+      <path d="M4 8H12" stroke="white" strokeWidth="2" strokeLinecap="round" />
     </svg>
   );
 }
