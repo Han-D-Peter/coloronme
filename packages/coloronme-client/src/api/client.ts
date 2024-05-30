2bbc181dfa201e5fb36cb2774c9b69c6a8208caa
365support <365support@naver.com>
2024-05-30 13:27:02 +0900

    feat: interceptors 추가

--- packages/coloronme-client/src/api/client.ts
+++ packages/coloronme-client/src/api/client.ts
@@ -1,13 +1,24 @@
-import Axios, { AxiosRequestConfig } from "axios";
-import { BASE_URL } from "../constants/constants";
+import Router from 'next/router';
+import Axios, { AxiosRequestConfig } from 'axios';
+import { BASE_URL } from '../constants/constants';
 
 const axios = Axios.create({
   baseURL: `${BASE_URL}/`,
   withCredentials: true,
   headers: {
-    "Content-Type": "application/json",
+    'Content-Type': 'application/json',
   },
 });
+
+axios.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      Router.replace('/');
+    }
+    return Promise.reject(error);
+  },
+);
 
 const client = {
   async get(url: string, config?: AxiosRequestConfig) {
