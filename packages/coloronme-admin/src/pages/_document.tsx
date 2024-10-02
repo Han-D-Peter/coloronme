6e24b1bc29c53ccc9759feb21fee2ff30275ebbc
Han-D-Peter <57760806+Han-D-Peter@users.noreply.github.com>
2024-10-02 22:27:04 +0900

    fix: viewport meta added

--- packages/coloronme-admin/src/pages/_document.tsx
+++ packages/coloronme-admin/src/pages/_document.tsx
@@ -5,6 +5,7 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="manifest" href="/manifest.json" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
       </Head>
       <body>
         <Main />
