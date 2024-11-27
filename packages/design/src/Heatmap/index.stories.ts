6dab91645322e5f966bf183d3f8a31d3802229a3
Han-D-Peter <secret_wings@naver.com>
2024-11-27 09:21:33 +0900

    fix: storybook fixed

--- packages/design/src/Heatmap/index.stories.ts
+++ packages/design/src/Heatmap/index.stories.ts
@@ -1,46 +1,32 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
-import Pie from './index';
+import Heatmap from './index';
 
 const meta = {
-  title: 'Example/Pie',
-  component: Pie,
+  title: 'Example/Heatmap',
+  component: Heatmap,
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
   tags: ['autodocs'],
   parameters: {
     // More on how to position stories at: https://storybook.js.org/docs/react/configure/story-layout
     layout: 'centered',
   },
-} satisfies Meta<typeof Pie>;
+} satisfies Meta<typeof Heatmap>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Primary: Story = {
   args: {
+    min: 0,
+    max: 100,
     width: 300,
     height: 200,
     data: [
-      {
-        label: 'Best_SW_LG',
-        value: 'Best_SW_LG',
-        count: 19,
-      },
-      {
-        label: 'Best_SC_MT',
-        value: 'Best_SC_MT',
-        count: 19,
-      },
-      {
-        label: 'Best_FW_DP',
-        value: 'Best_FW_DP',
-        count: 19,
-      },
-      {
-        label: 'Best_WC_DP',
-        value: 'Best_WC_DP',
-        count: 19,
-      },
+      { id: 'unique', data: [{ x: 'Best_SW_LG', y: 19 }] },
+      { id: 'unique', data: [{ x: 'Best_SW_LG', y: 19 }] },
+      { id: 'unique', data: [{ x: 'Best_SW_LG', y: 19 }] },
+      { id: 'unique', data: [{ x: 'Best_SW_LG', y: 19 }] },
     ],
   },
 };
